f70c46029061b47b01db8f388171985dd56b67bd
OmarElzero <omarmohmed7659944@gmail.com>
2023-09-07 18:53:29 -0700

    omar has been tired

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main -check it dude
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
-char str[] -""Programming is like building a multilingual puzzle\n";
+char str[] -"\"Programming is like building a multilingual puzzle\n";
 puts(str);
 return (0);
+
 }
