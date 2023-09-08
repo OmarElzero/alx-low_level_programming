2b0b1675fbd6a4aca673ef38db5dc7da26790950
OmarElzero <omarmohmed7659944@gmail.com>
2023-09-08 14:58:30 -0700

    ya mosahel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,9 +9,16 @@ int main(void)
 int i;
 double d;
 char c;
+long int li;
+long long int lli;
+float f;
+
 	printf("the size of an int is:%lu.\n", (unsigned long)sizeof(i));
 	printf("the size of an double is:%lu.\n", (unsigned long)sizeof(d));
 	printf("the size of an char is:%lu.\n", (unsigned long)sizeof(c));
+	printf("the size of a long int is:%lu.\n", (unsigned long)sizeof(li));
+	printf("the size of a long long int is:%lu.\n", (unsigned long)sizeof(lli));
+	printf("the size of a float is:%lu.\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
