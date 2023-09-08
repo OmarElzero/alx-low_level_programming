6fbd41ec11da2113d18b5edc464f8280782da70f
OmarElzero <omarmohmed7659944@gmail.com>
2023-09-08 15:42:07 -0700

    ya mosahel

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,23 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
+/**
+ * main - generates a random number and prints its sign
+ *
+ * Return: 0 if the program completes successfully
+ */
 int main(void)
 {
     int n;
 
+    /* Initializes the random number generator with the current time */
     srand(time(0));
+
+    /* Generate a random integer between -2147483647 and +2147483647 */
     n = rand() - RAND_MAX / 2;
 
+    /* Prints the sign of the random integer using if/else statements */
     if (n > 0)
     {
         printf("%d is positive\n", n);
