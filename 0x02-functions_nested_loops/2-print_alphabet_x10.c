776c54fb9cefc415d732569aa00833f925b4d644
OmarElzero <omarmohmed7659944@gmail.com>
2023-09-13 13:06:29 -0700

    07

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,12 @@
  */
 void print_alphabet_x10(void)
 {
-	for (int b = 0; b < 10; b++)
+	int i, b;
+	for ( b = 0; b < 10; b++)
 {
-	for (int i = 97; i <= 122; i++)
+	for ( i = 97; i <= 122; i++)
 {
-	_putchar(i);
+	_putchar('0'+i);
 }
 
 }
