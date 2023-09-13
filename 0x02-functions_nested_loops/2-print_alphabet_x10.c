c50b41a0a500b6fa856289c87a12ca9d9dd29a69
OmarElzero <omarmohmed7659944@gmail.com>
2023-09-13 09:32:46 -0700

    04

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main-print ascii in lower case x10
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+        for(int b = 0; b < 9; b++)
+{
+        for(int i = 97; i<= 122; i++)
+{
+	_putchar(i);
+
+}
+         _putchar('\n');
+}
