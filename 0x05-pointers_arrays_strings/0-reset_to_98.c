fd3f85587c579777208b47e111b5ac257e57480b
OmarElzero <omarmohmed7659944@gmail.com>
2023-10-12 15:45:28 -0700

    let's do it

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - takes a pointer to a parameter and updates its value
  *main - check the code
- *#n :intailzed integer 
+ *@n:intailzed integer input
  *Return: Always 0
  */
 void reset_to_98(int *n)
