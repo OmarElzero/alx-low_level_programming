d892d1c8058ba73b9e3179bdd94287e258a6b625
OmarElzero <omarmohmed7659944@gmail.com>
2023-10-26 19:36:42 -0700

    deadline is coming

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,14 +8,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 	 unsigned int num;
-	if(!b)
+	if (!b)
 	return (0);
-	while(*b != '\0')
+	while (*b != '\0')
 {
-	if(*b != '0' && *b != '1')
-	{return (0);}
+	if (*b != '0' && *b != '1')
+	{ return (0); }
 	num *= 2;
-	num += (*b-'0');
+	num += (*b - '0');
 	b++;
 }
 return (num);
