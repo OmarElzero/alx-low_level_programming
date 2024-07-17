94eabc5307dc1074015c98b1f8495b1cb461a37f
root <root@WIN-7E8AJGMQ4BP.localdomain>
2024-07-17 22:06:50 +0300

    linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+/**
+  * linear_search - Searches for a value in a sorted array
+  *               of integers using linear search
+  * @array: ptr to first element of the array to search
+  * @size: number of elements in the array
+  * @value: value to search for
+  * Return: If the value is not present or the array is NULL, -1
+  *         Otherwise, the first index where the value is located
+  * Description: Prints a value every time it loops in the array
+  *        
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t x;
+
+	if(array == NULL || size == 0)
+		return (-1);
+
+
+	for (x = 0; x < size; x++)
+	{
+         if(array[x]== value)
+        {
+            printf("Found [%ld] at index: [%d]\n", x, array[x]);
+            break;
+        }
+        
+		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+       
+		
+	}
+}
